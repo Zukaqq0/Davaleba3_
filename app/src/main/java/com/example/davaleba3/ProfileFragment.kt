e7259667f778591052baa205d547206d58ce69da
Zuka <kolbaia_zuka@mail.ru>
2023-05-17 21:21:59 +0400

    Initial commit

---
+++ app/src/main/java/com/example/davaleba3/ProfileFragment.kt
@@ -0,0 +1,40 @@
+package com.example.davaleba3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val tv :EditText = view.findViewById(R.id.profileFragment)
+        val text = arguments?.getString("Myarg")
+
+
+    }
+
+
+}
